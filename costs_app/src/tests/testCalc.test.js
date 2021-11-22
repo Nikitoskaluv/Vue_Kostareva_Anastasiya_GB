06581a6fc9242fccbeff3de21f4e8af3f90c87f8
Nikitoskaluv <anastasiyak90@mail.ru>
2021-11-22 18:03:34 +0300

    Test VuexLesson

--- costs_app/src/tests/testCalc.test.js
+++ costs_app/src/tests/testCalc.test.js
@@ -1,3 +1,34 @@
 import { mount } from "@vue/test-utils";
 import CalcComponent from "../components/CalcComponent.vue";
 
+describe('Test Calc', () => {
+    it('Test first operand input', () => {
+        const wrapper = mount(CalcComponent);
+        const operand1 = wrapper.find('input[name=operand1]');
+        operand1.setValue('1');
+        expect(wrapper.vm.operand1).toBe(1);
+    });
+    it('Test second operand input', () => {
+        const wrapper = mount(CalcComponent);
+        const operand2 = wrapper.find('input[name=operand2]');
+        operand2.element.value = "1";
+        operand2.trigger('input');
+        expect(wrapper.vm.operand2).toBe(1);
+    });
+    it('Test summ', () => {
+        const wrapper = mount(CalcComponent);
+
+        const operand2 = wrapper.find('input[name=operand2]');
+        operand2.element.value = "2";
+        operand2.trigger('input');
+
+        const operand1 = wrapper.find('input[name=operand1]');
+        operand1.setValue('3');
+
+        const btn = wrapper.find('button[title="+"]');
+        btn.trigger('click')
+
+
+        expect(wrapper.vm.result).toBe(5);
+    });
+});
